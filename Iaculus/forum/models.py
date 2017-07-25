d08d957d0a507bb4198d9eb0c48f72a937d5a0f1
safa <safafaydali@gmail.com>
2017-07-25 17:25:16 +0300

    models.py added Category, Topic, Post fields.

--- Iaculus/forum/models.py
+++ Iaculus/forum/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default="")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    forum = models.ForeignKey(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    closed = models.BooleanField(blank=True, default=False)
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    topic = models.ForeignKey(Topic)
+    body = models.TextField()
