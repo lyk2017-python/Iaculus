3db4c4ddb2a72008e9814d2a72b44300369d638b
safa <safafaydali@gmail.com>
2017-07-26 10:54:39 +0300

    SSS page added to urls.py, added methods to views.py

--- Iaculus/forum/urls.py
+++ Iaculus/forum/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from forum.views import HomepageView, TopicView, PostView
+from forum.views import HomepageView, TopicView, PostView, SSSView
 
 urlpatterns = [
-    url(r'^$', HomepageView),
-    url(r'^topic/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', TopicView),
-    url(r'^post/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', PostView)
+    url(r'^$', HomepageView, name="home"),
+    url(r'^topic/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', TopicView, name="topic"),
+    url(r'^post/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', PostView, name="post"),
+    url(r'^sss$', SSSView, name="faq"),
 ]
--- Iaculus/forum/views.py
+++ Iaculus/forum/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from forum.models import Category, Topic, Post
 
 
 class HomepageView(generic.ListView):
     model = Category
 
 class TopicView(generic.ListView):
-
+    model = Topic
 
 class PostView(generic.DetailView):
+    def get_queryset(self):
+        return Post.objects.all()
+
+class SSSView(generic.TemplateView):
+    template_name = "blog/sss.html"
