81216ab405c5e8f078ce13063312f46fd230d5a5
safa <safafaydali@gmail.com>
2017-07-26 11:33:43 +0300

    as_view() added

--- Iaculus/forum/urls.py
+++ Iaculus/forum/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from forum.views import HomepageView, TopicView, PostView, SSSView
 
 urlpatterns = [
-    url(r'^$', HomepageView, name="home"),
-    url(r'^topic/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', TopicView, name="topic"),
-    url(r'^post/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', PostView, name="post"),
-    url(r'^sss$', SSSView, name="faq"),
+    url(r'^$', HomepageView.as_view(), name="home"),
+    url(r'^topic/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', TopicView.as_view(), name="topic"),
+    url(r'^post/(?P<id>\d+)-(?P<slug>[A-za-z0-9\-]+)$', PostView.as_view(), name="post"),
+    url(r'^sss$', SSSView.as_view(), name="faq"),
 ]
